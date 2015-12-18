e6f908252ddfa2c3361173557707b42f3d8f84f2
James Lett <jlett89@gmail.com>
2015-12-17 17:14:20 -0800

    Add style reverted changes in html add logic in js
    Change around the html after poking and not finding a solution. Trying
    a different way to get UTC time to show

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
-angular.module("schedule1", []).controller("Scheduler", ["$http", function($http) {
-  this.jobs = []
+var app = angular.module('myApp');
 
-}]),
+app.controller('taskController', function($scope, $rootScope) {
+
+  $scope.tasks = tasks;
+
+  $scope.editTask = function (task) {
+    console.log(task);
+  };
+
+  $scope.remove = function($index) {
+    console.log("REMOVED");
+    $scope.tasks.splice($index, 1);
+  };
+
+  $scope.addTask = function(task) {
+    console.log("WORKING ON TASK");
+    console.log(task);
+    tasks.push(task);
+    $scope.task = null
+  };
+
+  $scope.saveTask = function(){
+  };
+
+  $scope.cancel = function(){
+    $scope.task = null;
+  }
+
+  $scope.date = new Date();
+  $scope.date.setDate($scope.date.getDate() + 1);
+})
